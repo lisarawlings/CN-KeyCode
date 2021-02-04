3fae775afdc7a1ed7bdae77fb0344e38f4e9a3bc
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-02-04 14:48:19 +0000

    update

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const welcome = document.getElementById("welcomepage");
+const generator = document.getElementById("generator");
+const head = document.getElementById("keycode") 
 
-keycode(event){
-
+function keycode() {
+    var key = event.keycode;
 }
+
+document.addEventListener("keydown", function(event) {
+    console.log(event.which);
+})
